228d943787e8ddd5a9ba1cb877bd5c26d1bd6134
AshuRaj7 <shoky093@gmail.com>
2025-01-07 16:57:55 +0530

    go button-responsive

--- src/component/header.js
+++ src/component/header.js
@@ -11,19 +11,19 @@ const Header = ({ toggleSidebar }) => {
         ☰
       </button>
 
-      {/* Centered Search Bar with Go Button */}
-      <div className="flex-grow flex justify-center">
-        <div className="flex items-center bg-gray-700 rounded-lg overflow-hidden">
+      {/* Responsive Centered Search Bar with Go Button */}
+      <div className="flex-grow flex justify-center items-center">
+        <div className="flex flex-col sm:flex-row items-center bg-gray-700 rounded-lg overflow-hidden p-2 sm:p-0">
           <input
             type="search"
             placeholder="Search..."
-            className="bg-gray-700 text-white placeholder-gray-400 px-4 py-2 mr-4 focus:outline-none w-64"
+            className="bg-gray-700 text-white placeholder-gray-400 px-4 py-2 focus:outline-none w-full sm:w-64"
           />
           
+          
         </div>
-
         <button
-            className="text-black font-bold bg-white ml-2 px-4 py-2 rounded-lg hover:bg-green-400 transition duration-300 ease-in-out"
+            className="text-black font-bold bg-white mt-2 sm:mt-0 sm:ml-2 px-4 py-2 rounded-lg hover:bg-green-400 transition duration-300 ease-in-out"
             style={{ fontSize: 16 }}
           >
             Go
